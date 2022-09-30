7ffb323d6e8c03aabb7efa94a0fc125758603d5e
No Body <pinyoo.too@gmail.com>
2022-09-30 22:24:38 +0700

    Add function subscribe in client

--- src/client.py
+++ src/client.py
@@ -1,11 +1,15 @@
 import sys
 import socket
-import time
+import time,datetime
 import threading
+import argparse
 from lib.event import *
 from lib.parse import *
+from lib.handle import *
 
-class InternalClient:
+parser = argparse.ArgumentParser()
+
+class Client:
     def __init__(self,host = "127.0.0.1",port = 4000):
         self.host = host
         self.port = port
@@ -27,21 +31,36 @@ class InternalClient:
             self.sock.connect((self.host,self.port))
         except socket.error as e:
             print(str(e))
+
+    def display_message(self,payload):
+        try:
+            from_user = payload['from']
+            message = payload['message']
+            print("\n{} say : {}\r".format(from_user,message))
+        except KeyError as ke:
+            print("Key error : {}".format(ke))
+        except ValueError as ve:
+            print("Value error : {}".format(ve))
+
+    def handle_event(self,resp):
+        event,payload = handle_recieve_event(resp.decode())
+        if event == EVENT_RECIEVE_MESSAGE_FROM_USER:
+            # TODO :: Decrypt
+            self.display_message(payload)
     
     def handle_recieve(self):
         while self.do_runing:
             resp = self.sock.recv(self.BUFFER_SIZE)
             if resp:
-                print("{} :Received data : {}\n\r".format(self.count,resp.decode()))
-                self.count+=1
+                self.handle_event(resp)
             
             time.sleep(0.5)
 
     def handle_send(self):
         while self.do_runing:
-            data = input('{}:{}>'.format(self.host,self.port))
+            data = input('[{}]{}>'.format(self.get_user(),self.get_hostname()))
             if data:
-                print("Send data : {}\r".format(data))
+                print("\nSend data : {}\r".format(data))
                 self.sock.send(data.encode())
             time.sleep(0.2)
     
@@ -57,12 +76,18 @@ class InternalClient:
     def publish(self,cmd):
         self.sock.sendall(cmd.encode())
 
+    def subscribe(self):
+        resp = self.sock.recv(self.BUFFER_SIZE)
+        if resp:
+            return resp.decode()
+
     def join_lobbie(self):
         cmd = self.pack_command(
             EVENT_JOIN_LOBBIE,
             {
                 "user_id" : self.get_user(),
-                "host" : self.get_hostname()
+                "host" : self.get_hostname(),
+                "timestamp" : "{}".format(datetime.datetime.now())
             }
         )
         self.publish(cmd)
@@ -73,7 +98,8 @@ class InternalClient:
             {
                 "to" : to_user_id,
                 "from" : self.get_user(),
-                "message" : message
+                "message" : message,
+                "timestamp" : "{}".format(datetime.datetime.now())
             }
         )
         self.publish(cmd)
@@ -91,13 +117,15 @@ class InternalClient:
         print("Disconnect socket client user id : {}".format(self.user_id))
 
 if __name__ == "__main__":
-    client = InternalClient(host="127.0.0.1",port=4000)
-    try:
-        client.add_user("user_01")
-        client.join_lobbie()
-    
-        #client.send_message("user_01","Hello user 01")
+    parser.add_argument("-u", "--url", help = "Client connect host ",default='127.0.0.1')
+    parser.add_argument("-p", "--port", help = "Client connect port",default=5000)
+    parser.add_argument("-n", "--username", help = "User name",default="user_01")
+    args = parser.parse_args()
 
+    client = Client(host=args.url,port=int(args.port))
+    try:
+        client.add_user(args.username)
+        client.join_lobbie()
         client.run()
     except KeyboardInterrupt:
         print("Stop process..")
